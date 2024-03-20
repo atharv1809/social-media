134b3240162d905df206bfe376dd6fb1bc0c7454
atharv1809 <119276340+atharv1809@users.noreply.github.com>
2024-03-20 19:48:03 +0530

    Add files via upload

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,51 @@
+<<<<<<< HEAD
+import "bootstrap/dist/css/bootstrap.min.css"
+function CreatePost()
+{
+    return <form>
+    <div class="mb-3">
+      <label for="userId" class="form-label"> Enter your UserId</label>
+      <input type="text" class="form-control" id="userId" placeholder="Enter your userId."/>
+    </div>
+    <div class="mb-3">
+      <label for="title" class="form-label">Post Title</label>
+      <input type="text" class="form-control" id="title" placeholder="How are you feeling??"/>
+    </div>
+    <div class="mb-3">
+      <label for="body" class="form-label">Post Content</label>
+      <textarea type="text" class="form-control" id="body" placeholder="Tell us more about it." rows="4"/>
+    </div>
+    <div class="mb-3">
+      <label for="reactions" class="form-label">Number of Reactions</label>
+      <input type="text" class="form-control" id="reactions" placeholder="Enter total number of reactions."/>
+    </div>
+    <div class="mb-3">
+      <label for="tags" class="form-label">Tags</label>
+      <input type="text" class="form-control" id="tags" placeholder="Enter tags with space."/>
+    </div>
+    <button type="submit" class="btn btn-primary">Post</button>
+  </form>
+}
+=======
+import "bootstrap/dist/css/bootstrap.min.css"
+function CreatePost()
+{
+    return <form>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1"/>
+    </div>
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+}
+>>>>>>> 601622e0c0c7801ab571a42189d66ebe843e6009
+export default CreatePost;
---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,94 @@
+<<<<<<< HEAD
+import { createContext, useReducer } from "react";
+export const PostListContext=createContext({
+    postList: [],
+    addPost: () => {},
+    deletePost: () => {},
+});
+const postListReducer=(currPostList,action)=>
+{
+    let newPostList=currPostList;
+    if(action.type==='DELETE_POST'){
+        newPostList=currPostList.filter((post)=>post.id!==action.payload.postId);
+    }
+    return newPostList;
+}
+function PostListProvider({children})
+{
+    const addPost=()=>
+    {
+
+    }
+    const deletePost=(postId)=>
+    {
+        dispatchPostList({type: 'DELETE_POST',payload: {postId: postId}});
+    }
+    const [postList,dispatchPostList]=useReducer(postListReducer,DEFAULT_POST_LIST);
+    return <PostListContext.Provider value={{postList: postList, addPost: addPost, deletePost: deletePost}}>
+        {children}
+    </PostListContext.Provider>
+}
+const DEFAULT_POST_LIST=[
+    {
+        id: "1",
+        title: "Going to Mumbai",
+        body: "My body 1",
+        reactions: 2,
+        userId: "user-9",
+        tags: ["vacation","Mumbai","Enjoying"],
+    },
+    {
+        id: "2",
+        title: "Going to Pune",
+        body: "My body 2",
+        reactions: 100,
+        userId: "user-5",
+        tags: ["Internship","Pune","Enjoying"],
+    },
+];
+=======
+import { createContext, useReducer } from "react";
+export const PostListContext=createContext({
+    postList: [],
+    addPost: () => {},
+    deletePost: () => {},
+});
+const postListReducer=(currPostList,action)=>
+{
+    return currPostList;
+}
+function PostListProvider({children})
+{
+    const addPost=()=>
+    {
+
+    }
+    const deletePost=()=>
+    {
+
+    }
+    const [postList,dispatchPostList]=useReducer(postListReducer,DEFAULT_POST_LIST);
+    return <PostListContext.Provider value={{postList: postList, addPost: addPost, deletePost: deletePost}}>
+        {children}
+    </PostListContext.Provider>
+}
+const DEFAULT_POST_LIST=[
+    {
+        id: "1",
+        title: "Going to Mumbai",
+        body: "MY body 1",
+        reactions: 2,
+        userId: "user-9",
+        tags: ["vacation","Mumbai","Enjoying"],
+    },
+    {
+        id: "2",
+        title: "Going to Pune",
+        body: "MY body 2",
+        reactions: 100,
+        userId: "user-5",
+        tags: ["Internship","Pune","Enjoying"],
+    },
+];
+>>>>>>> 601622e0c0c7801ab571a42189d66ebe843e6009
+export default PostListProvider;
