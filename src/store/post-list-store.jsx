82dce37f0cf67bfc85a240aad23049df518e7f1e
Atharv Bhadange <atharv1809>
2024-03-20 14:37:05 +0530

    my commit

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,10 +1,25 @@
-import { createContext } from "react";
-
-const PostList=createContext({});
+import { createContext, useReducer } from "react";
+const PostList=createContext({
+    postList: [],
+    addPost: () => {},
+    deletePost: () => {},
+});
+const postListReducer=(currPostList,action)=>
+{
+    return currPostList;
+}
 function PostListProvider({children})
 {
-    const [postList,dispatchPostList]=useReducer();
-    return <PostList.Provider value={[]}>
+    const addPost=()=>
+    {
+
+    }
+    const deletePost=()=>
+    {
+
+    }
+    const [postList,dispatchPostList]=useReducer(postListReducer,[]);
+    return <PostList.Provider value={{postList: postList, addPost: addPost, deletePost: deletePost}}>
         {children}
     </PostList.Provider>
 }
